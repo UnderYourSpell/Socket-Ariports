0638e289ed67d2694cc9e8386afcb593ad07902c
Moro Bamber <mwbamber@alaska.edu>
2023-10-06 15:17:32 -0800

    Pretty much done

--- libs/client.py
+++ libs/client.py
@@ -1,12 +1,14 @@
 import socket
-import threading
+
+'''
+This code supports clients in our system
+'''
 
 SIZE = 1024
 FORMAT = "utf-8"
 
 
 class Client:
-
 
     def __init__(self,sock=None,name = str):
         if sock is None:
@@ -21,7 +23,10 @@ class Client:
 
     #def function to send message, already kind of written in run
     #def function to check if airport is allowed
-    def init_allowed_airports(self,airports):
+
+    def init_allowed_airports(self,airports): #tell the client where it can send something
+        #in our client files, the while loop will default mostly to Anchorage
+        #PDX defaults to SEA
         for item in airports:
             self.allowed_airports.append(item)
 
@@ -34,7 +39,7 @@ class Client:
         self.sock.connect(server_address)
         print(f"[CONNECTED] Client connected to server at {host}:{port}")
 
-    def update_info(self,dest,payload):
+    def update_info(self,dest,payload): #helper function I wrote while testing 
         self.dest = dest
         self.payload = payload
 
@@ -42,14 +47,16 @@ class Client:
         return self.dest
     
     def input_message(self):
+        #allows for input from command line
         self.cont = input("Enter . to quit, press any key to continue ")
         self.dest = input("Destination: ")
         self.payload = input("Passenger Name: ")
 
-    def send(self):
+    def send(self): #send message function
             dest  = self.dest
             payload = self.payload
             #delimiter = '\n'
+            #Send the message using delimeters to the server can differentiate what information it recieves
             message = self.type + '\n' + self.name + '\n' + dest + '\n' + payload + '\n'
             print(f"SENDING {payload} TO {dest}")
             self.sock.send(message.encode(FORMAT))
--- libs/server.py
+++ libs/server.py
@@ -2,6 +2,10 @@ import socket
 import threading
 from .airport_codes import AirportCodes
 
+'''
+This code supports the servers in our system
+'''
+
 
 SIZE = 1024
 FORMAT = "utf-8"
@@ -25,11 +29,11 @@ class Server():
             self.accepted_airports = []
             self.run_server = 1
 
-    def declare_accepted_aiports(self,airports):
+    def declare_accepted_aiports(self,airports): #tell the server what airports it can connect to
         for code in airports:
             self.accepted_airports.append(code)
 
-    def bind(self,host,port):
+    def bind(self,host,port): #bind to a port
         self.server_address = (host,port)
         self.host = host
         self.port = port
@@ -39,6 +43,7 @@ class Server():
         delimiter = '\n'
         print(f"[NETWORK CONNECTION] {addr} connected.")
 
+        #receive the data, and parse the information that was sent
         data = conn.recv(SIZE).decode()
         info = data.split(delimiter)
         from_type = info[0]
@@ -46,15 +51,23 @@ class Server():
         dest = info[2]
         payload = info[3]
 
+        #Allows me to stop the server from running so I can save the Log file
         if payload == '!END_SERVER':
                 print("[SERVER STOP REQUEST RECEIVED]")
                 print("[STOPPING SERVER]")
                 self.run_server = 0
 
+        #Show who's connected and what their destination i
         print(f"{name} CONNECTED")
         print(f"DESTINATION: {dest}")
         
-        #handle from a server
+        '''handle from a server
+        in my server architecture, since we only have 1 layover
+        if a server forwards something, then the next server that receives it will
+        store the info
+
+        This server also recognizes itself as a client so it can forward a message to itself
+        ''' 
         if from_type == "s":
             self.received_payloads.append([name,payload])
             print(f"{payload} HAS ARRIVED IN: {dest} FROM: {name}")
@@ -70,6 +83,9 @@ class Server():
                 
             #print(self.connections)
 
+            #if the airport exists in what airports we can go to
+            #send a message as a new client
+            #it connects the passenger to the final destination
             for airport in self.accepted_airports:
                     if dest == airport:
                         target_host, target_port = self.airport_codes.get_address(dest)
@@ -89,12 +105,14 @@ class Server():
                         
         conn.close()
 
-    def run(self):
+    def run(self): #starts the server's listenting function
+        #allows for multiple connections
         print("Starting server")
         self.sock.listen()
         print(f"Listenting on {self.host}:{self.port}")
 
-
+        #main body that accepts things into the server
+        #uses the client handle function to do server operations
         while self.run_server == 1:
             conn, addr = self.sock.accept()
             new_thread = threading.Thread(target = self.handle_client, args = (conn,addr))
